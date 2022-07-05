428f5545d78a1e4454c9a194a59f71fbc3c4e9f3
kodaishiotsuki <0711dr5@gmail.com>
2022-07-05 16:21:04 +0900

    オンデマンド ISR ページの再検証(commentページ)

---
+++ hooks/useMutateComment.ts
@@ -0,0 +1,77 @@
+import { useMutation } from 'react-query'
+import useStore from '../store'
+import { Comment, EditedComment } from '../types/types'
+import { supabase } from '../utils/supabase'
+import { revalidateSingle } from '../utils/revalidation'
+
+//カスタムフック作成
+export const useMutateComment = () => {
+  const reset = useStore((state) => state.resetEditedComment)
+
+  //commentの新規作成
+  const createCommentMutation = useMutation(
+    async (comment: Omit<Comment, 'created_at' | 'user_id' | 'note_id'>) => {
+      const { data, error } = await supabase.from('comments').insert(comment)
+      if (error) throw new Error(error.message)
+      return data
+    },
+    {
+      onSuccess: (res) => {
+        revalidateSingle(res[0].note_id) //個別ページのみ再生成
+        reset() //初期化
+        alert('Successfully completed!!')
+      },
+      onError: (err: any) => {
+        alert(err.message)
+        reset()
+      },
+    }
+  )
+
+  //commentの更新
+  const updateCommentMutation = useMutation(
+    async (comment: EditedComment) => {
+      const { data, error } = await supabase
+        .from('comments')
+        .update({ content: comment.content })
+        .eq('id', comment.id)
+      if (error) throw new Error(error.message)
+      return data
+    },
+    {
+      onSuccess: (res) => {
+        revalidateSingle(res[0].note_id)
+        reset() //初期化
+        alert('Successfully completed!!')
+      },
+      onError: (err: any) => {
+        alert(err.message)
+        reset()
+      },
+    }
+  )
+
+  //commentの削除
+  const deleteCommentMutation = useMutation(
+    async (id: string) => {
+      const { data, error } = await supabase
+        .from('comments')
+        .delete()
+        .eq('id', id)
+      if (error) throw new Error(error.message)
+      return data
+    },
+    {
+      onSuccess: (res) => {
+        revalidateSingle(res[0].note_id)
+        reset() //初期化
+        alert('Successfully completed!!')
+      },
+      onError: (err: any) => {
+        alert(err.message)
+        reset()
+      },
+    }
+  )
+  return { deleteCommentMutation, createCommentMutation, updateCommentMutation }
+}
